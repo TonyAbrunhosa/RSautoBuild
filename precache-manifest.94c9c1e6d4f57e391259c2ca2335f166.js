125cef776634241d7a641e64b39231af1536200a
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2023-05-08 12:38:59 -0300

    Initial commit

---
+++ precache-manifest.94c9c1e6d4f57e391259c2ca2335f166.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21b64ab4a06d2e07369705c08a870ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a3315fb6e29d1c06f4c",
+    "url": "/static/css/2.1d8ee416.chunk.css"
+  },
+  {
+    "revision": "2a3315fb6e29d1c06f4c",
+    "url": "/static/js/2.25bc57fc.chunk.js"
+  },
+  {
+    "revision": "2d86931df81c9fdc26a4b8d231728fcb",
+    "url": "/static/js/2.25bc57fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edacf2d62fe4d6872014",
+    "url": "/static/js/main.2f80c268.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2f80c268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b55aa52587cb70257e",
+    "url": "/static/js/runtime-main.5d2bdb24.js"
+  },
+  {
+    "revision": "dfe5699fca8b3305a603bb5bbe999a19",
+    "url": "/static/media/cars.dfe5699f.png"
+  },
+  {
+    "revision": "335a94604c59d2ea0c308c754ae423f1",
+    "url": "/static/media/logo.335a9460.svg"
+  },
+  {
+    "revision": "429b2aefaea3e0b0c181c74e96dc7026",
+    "url": "/static/media/order.429b2aef.png"
+  },
+  {
+    "revision": "c19ed2c2f46e3ac90619d76f8f534e05",
+    "url": "/static/media/part.c19ed2c2.png"
+  },
+  {
+    "revision": "40993bc3ea67f9bb52393cd1fe4086d7",
+    "url": "/static/media/parts.40993bc3.png"
+  }
+]);
